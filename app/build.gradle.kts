c66733b6e85058285cb8660d58e09a08c3906605
James Robinson <computers.wreck.your.eyesight@gmail.com>
2026-02-17 13:28:33 -0800

    Do a nothing updaate to satisfy Google.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.navigatebyfaith.rrreadings"
         minSdk = 26
         targetSdk = 36
-        versionCode = 134040100
-        versionName = "4.1.0"
+        versionCode = 134040102
+        versionName = "4.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
